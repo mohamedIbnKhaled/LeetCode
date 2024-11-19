d99cb415f6b2ad0530f0e5dfda1e52bde5f5fead
محمد خالد عصام <mohameddkee@gmail.com>
2024-11-19 11:33:48 +0200

    Time: 3 ms (13.96%), Space: 17.7 MB (12.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -5,25 +5,19 @@ public:
     bool check(int x,int y, int m,int n){
         return x>=0&&x<m&&y>=0&&y<n;
     }
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color,vector<vector<bool>>&vis){
-        vis[sr][sc]=1;
+    map<pair<int,int>,bool>vis;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+         vis[{sr,sc}]=1;
         int prVal=image[sr][sc];
         image[sr][sc]=color;
            for(int i=0;i<4;i++){
             int nx,ny;
             nx=dx[i]+sr;
             ny=dy[i]+sc;
-            if(check(nx,ny,image.size(),image[sr].size())&&!vis[nx][ny]&&image[nx][ny]==prVal){
-                dfs(image,nx,ny,color,vis);
+            if(check(nx,ny,image.size(),image[sr].size())&&!vis[{nx,ny}]&&image[nx][ny]==prVal){
+                floodFill(image,nx,ny,color);
             }
         }
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<bool>>vis(image.size());
-        for(int i=0;i<image.size();i++){
-            vis[i]=vector<bool>(image[i].size());
-        }
-        dfs(image,sr,sc,color,vis);
         return image;
     }
 };
