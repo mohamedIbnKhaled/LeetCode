098febee158149cc317dd01f25181d02e441e0c0
محمد خالد عصام <mohameddkee@gmail.com>
2024-12-25 12:01:33 +0200

    Time: 1 ms (45.83%), Space: 62.9 MB (27.52%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left=0,right=height.size()-1;
+        int ans=(right-left)*min(height[left],height[right]);
+        while(left<right){
+            ans=max(ans,(right-left)*min(height[left],height[right]));
+            if(height[left]<height[right])left++;
+            else right--;
+            
+        }
+        return ans;
+    }
+};
